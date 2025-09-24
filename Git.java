8dc32e1f31c93c38d00cefc1d370db88096b2f84
Audrey McLeod <amcleod1@hwemail.com>
2025-09-23 20:57:42 -0700

    (GP-2.1.1) Repository Initialization and Testing
    Program can initialize repository, and tester has been created. Both are documented in README.

--- Git.java
+++ Git.java
@@ -1,5 +1,5 @@
 import java.io.*;
-import java.nio.file.Files;
+import java.nio.file.*;
 
 public class Git {
 
@@ -10,29 +10,47 @@ public class Git {
 
     public Git() throws IOException {
         git = new File(".aGit");
-        objects = new File("/.aGit/objects");
-        index = new File("/.aGit/index");
-        HEAD = new File("/.aGit/HEAD");
+        objects = new File(git.getAbsolutePath() + File.separator + "objects");
+        index = new File(git.getAbsolutePath() + File.separator + "index");
+        HEAD = new File(git.getAbsolutePath() + File.separator + "HEAD");
+        initialize();
+    }
+
+    public Git(String folderName) throws IOException {
+        if (folderName.equals("git")) {
+            System.out.println("\".git\" folder already exists, using \".aGit\" instead.");
+            folderName = "aGit";
+        }
+        git = new File("."+folderName);
+        objects = new File(git.getAbsolutePath() + File.separator + "objects");
+        index = new File(git.getAbsolutePath() + File.separator + "index");
+        HEAD = new File(git.getAbsolutePath() + File.separator + "HEAD");
         initialize();
     }
 
     public void initialize() throws IOException {
-        if (!git.exists()) {
-            git.mkdir();
-        }
-        if (!objects.exists()) {
-            objects.mkdir();
-        }
-        if (!index.exists()) {
-            index.createNewFile();
-        }
-        if (!HEAD.exists()) {
-            HEAD.createNewFile();
-        }
-        if ((git.exists()&&objects.exists())&&(index.exists()&&HEAD.exists())) {
+        if ((git.exists() && objects.exists()) && (index.exists() && HEAD.exists())) {
             System.out.println("Git Repository Already Exists");
         } else {
+            if (!git.exists()) {
+                git.mkdir();
+            }
+            Path path = git.toPath();
+            Files.setAttribute(path, "dos:hidden", true);
+            if (!objects.exists()) {
+                objects.mkdir();
+            }
+            if (!index.exists()) {
+                index.createNewFile();
+            }
+            if (!HEAD.exists()) {
+                HEAD.createNewFile();
+            }
             System.out.println("Git Repository Created");
         }
+    }
+
+    public String getPath() {
+        return git.getAbsolutePath();
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# What Do You Call a Stupid British Person Who Stages Your Changes
+What Do You Call a Stupid British Person Who Stages Your Changes?
+
+READ ME:
+
+Git:
+Two options for creating the repository, if no parameters given, name will be ".aGit", otherwise name will be as specified when made.
+File.separator is used for paths in order to ensure code works on multiple operating systems, same with naming and hiding folder.
+
+GitTester:
+Method to verify if all required repo files were initialized (.repoExists)
+Method that iterates through folder and removes all files, including anything potentially added later in project process (.cleanup)
+Multiple tests to see if different cases produce errors. Details documented in tester.
+
