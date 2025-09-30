967ba57c241875c62136c3603b518b980be26e4d
Miles Aguilar <maguilar1@hwemail.com>
2025-09-30 14:23:26 -0700

    (GP-3.0.1)
    Added directory of test text files to make testing easier

--- src.java
+++ src.java
@@ -3,5 +3,6 @@ import java.io.IOException;
 public class src {
     public static void main(String[] args) throws IOException {
         Git testGit = new Git();
+        testGit.addFile(new File(""));
     }
 }
